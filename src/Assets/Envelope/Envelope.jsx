f9d16841542b37432f248dd27486072aed7d8393
ComsIndeed <TrulyComs2023@gmail.com>
2024-04-02 21:15:12 +0800

    Got the envelope to work

--- src/Assets/Envelope/Envelope.jsx
+++ src/Assets/Envelope/Envelope.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import EnvelopeSVG from "./Envelope.svg";
 
-export default function Envelope({ children }) {
+export default function Envelope({ children, className }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className="Envelope">
-        <EnvelopeSVG />
+      <div
+        className={`Envelope ${className} ${isOpen ? "open" : ""}`}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <EnvelopeSVG isOpen={isOpen}>{children}</EnvelopeSVG>
       </div>
     </>
   );
--- src/Stages/LandingStage.jsx
+++ src/Stages/LandingStage.jsx
@@ -1,10 +1,14 @@
 import Envelope from "../Assets/Envelope/Envelope";
+import Paper from "../Assets/Envelope/Paper";
+import "animate.css";
 
 export default function LandingStage() {
   return (
     <>
       <div className="LandingStage">
-        <Envelope />
+        <Envelope className="animate__animated animate__fadeInUp">
+          <Paper>Wassup sino man yan</Paper>
+        </Envelope>
       </div>
     </>
   );
