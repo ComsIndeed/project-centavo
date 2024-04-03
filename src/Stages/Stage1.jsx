add1c00e8e767a9403afbffab366af38b54a4a0d
ComsIndeed <TrulyComs2023@gmail.com>
2024-04-03 21:24:42 +0800

    Font and Scrolling test

--- src/Stages/Stage1.jsx
+++ src/Stages/Stage1.jsx
@@ -5,19 +5,36 @@ import Trees2 from "../Assets/Backgrounds/Stage1/trees2.png";
 import Trees3 from "../Assets/Backgrounds/Stage1/trees3.png";
 import Sun from "../Assets/Backgrounds/Stage1/sun.png";
 import Mountains from "../Assets/Backgrounds/Stage1/mountains.png";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Stage1() {
   return (
     <>
       <div className="Stage1 animate__animated animate__fadeIn">
         <div className="Page" id="Page1">
-          <h1>This page has content</h1>
+          <h1>YO YO WASSUP BRO</h1>
           <div className="Backgrounds">
-            <img src={Sun} />
-            <img src={Mountains} />
-            <img src={Trees3} />
-            <img src={Trees2} />
-            <img src={Trees1} />
+            <img
+              className="animate__animated animate__fadeInUp animate__delay-2s"
+              src={Sun}
+            />
+            <img
+              className="animate__animated animate__fadeInUp animate__delay-1s"
+              src={Mountains}
+            />
+            <img
+              className="animate__animated animate__fadeInUp "
+              src={Trees3}
+            />
+            <img
+              className="animate__animated animate__fadeInUp "
+              src={Trees2}
+            />
+            <img
+              className="animate__animated animate__fadeInUp "
+              src={Trees1}
+            />
           </div>
         </div>
         <div className="Page" id="Page2">
@@ -40,7 +57,37 @@ export default function Stage1() {
           <h1>This page has content</h1>
           <div className="Backgrounds"></div>
         </div>
+        <div className="Page" id="FormPage">
+          <FormPageContent />
+        </div>
       </div>
+    </>
+  );
+}
+
+function FormPageContent() {
+  const [handleMouseMove, setHandleMouseMove] = useState(() => {});
+
+  return (
+    <>
+      <h1>Would you?</h1>
+      <div className="form">
+        <button id="YesButton">Yes</button>
+        <button
+          id="NoButton"
+          onMouseMove={handleMouseMove}
+          onTouchStart={(event) => event.preventDefault()} // Prevent default touch behavior
+        >
+          No
+        </button>
+      </div>
+      <label>Additional notes:</label>
+      <textarea
+        name="AdditionalNotes"
+        id="AdditionalNotes"
+        placeholder="You can write here"
+      ></textarea>
+      <div className="Backgrounds"></div>
     </>
   );
 }
